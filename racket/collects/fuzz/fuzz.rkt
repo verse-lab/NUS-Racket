f7476c5450936178bb289dc9850f1f2a6ac64cfc
Kiran Gopinathan <kirang@comp.nus.edu.sg>
2023-02-06 11:24:22 +0000

    removed redundant fuzz definition

--- racket/collects/fuzz/fuzz.rkt
+++ racket/collects/fuzz/fuzz.rkt
@@ -2,10 +2,4 @@
 
 (require (for-syntax racket/base))
 
-(define-syntax (lookup-macro-definition stx)
-  (syntax-case stx ()
-    ((_ id)
-     #`(displayln (format "hello ~s" #,(syntax->datum #'id)))
-     )
-    )
-  )
+
